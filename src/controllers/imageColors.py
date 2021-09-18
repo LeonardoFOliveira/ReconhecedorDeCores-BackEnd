763159462ca04c3598c9d82d7a0f41bbb4ac241f
Leonardo Fernandes de Oliveira <leonardoferoliver@hotmail.com>
2021-09-18 14:50:10 -0300

    melhorias para trazer um nome de cor mais amigavel e já traduzir por um dicionario

--- src/controllers/imageColors.py
+++ src/controllers/imageColors.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, jsonify, make_response
-from flask_restx import Api, Resource
+from flask import request, jsonify, make_response
+from flask_restx import Resource
 import colorgram
 import webcolors
-from googletrans import Translator
 
 from src.server.instance import server
 
@@ -26,13 +25,180 @@ class ImageColors(Resource):
         predominant_color = colorgram.extract(fileImage,1)[0]
         actual_name_color_en, closest_name_color_en = get_colour_name((predominant_color.rgb.r,predominant_color.rgb.g,predominant_color.rgb.b))
         
+        blue = 'Azul'
+        white = 'Branco'
+        aqua = 'Água'
+        beige = 'Bege'
+        black = 'Preto'
+        purple = 'Roxo'
+        brown = 'Marrom'
+        green = 'Verde'
+        orange = 'Laranja'
+        yellow = 'Amarelo'
+        red = 'Vermelho'
+        golden = 'Dourado'
+        grey = 'Cinza'
+        khaki = 'Caqui'
+        pink = 'Rosa'
+        maroon = 'Bordo'
+        ## Dicionario criado baseado nas cores mapeadas no css3, e a atribuindo a um nome de cor mais amigavel e já traduzido para pt-br
+        ## https://www.w3.org/TR/css-color-3/#svg-color
+        color_name_dict = {
+            'aliceblue': blue,
+            'antiquewhite': white,
+            'aqua': aqua,
+            'aquamarine': aqua,
+            'azure': blue,
+            'beige': beige,
+            'bisque': beige,
+            'black': black,
+            'blanchedalmond': beige,
+            'blue': blue,
+            'blueviolet': purple,
+            'brown': brown,
+            'burlywood': beige,
+            'cadetblue': blue,
+            'chartreuse': green,
+            'chocolate': brown,
+            'coral': orange,
+            'cornflowerblue': blue,
+            'cornsilk': yellow,
+            'crimson': red,
+            'cyan': aqua,
+            'darkblue': blue,
+            'darkcyan': aqua,
+            'darkgoldenrod': golden,
+            'darkgray': grey,
+            'darkgreen': green,
+            'darkgrey': grey,
+            'darkkhaki': khaki,
+            'darkmagenta': purple,
+            'darkolivegreen': green,
+            'darkorange': orange,
+            'darkorchid': purple,
+            'darkred': red,
+            'darksalmon': orange,
+            'darkseagreen': green,
+            'darkslateblue': blue,
+            'darkslategray': grey,
+            'darkslategrey': grey,
+            'darkturquoise': aqua,
+            'darkviolet': purple,
+            'deeppink': pink,
+            'deepskyblue': blue,
+            'dimgray': grey,
+            'dimgrey': grey,
+            'dodgerblue': blue,
+            'firebrick': red,
+            'floralwhite': white,
+            'forestgreen': green,
+            'fuchsia': pink,
+            'gainsboro': grey,
+            'ghostwhite': white,
+            'gold': golden,
+            'goldenrod': golden,
+            'gray': grey,
+            'green': green,
+            'greenyellow': green,
+            'grey': grey,
+            'honeydew': green,
+            'hotpink': pink,
+            'indianred': red,
+            'indigo': purple,
+            'ivory': beige,
+            'khaki': khaki,
+            'lavender': purple,
+            'lavenderblush': purple,
+            'lawngreen': green,
+            'lemonchiffon': yellow,
+            'lightblue': blue,
+            'lightcoral': orange,
+            'lightcyan': aqua,
+            'lightgoldenrodyellow': golden,
+            'lightgray': grey,
+            'lightgreen': green,
+            'lightgrey': grey,
+            'lightpink': pink,
+            'lightsalmon': orange,
+            'lightseagreen': green,
+            'lightskyblue': blue,
+            'lightslategray': grey,
+            'lightslategrey': grey,
+            'lightsteelblue': blue,
+            'lightyellow': yellow,
+            'lime': green,
+            'limegreen': green,
+            'linen': beige,
+            'magenta': purple,
+            'maroon': maroon,
+            'mediumaquamarine': aqua,
+            'mediumblue': blue,
+            'mediumorchid': purple,
+            'mediumpurple': purple,
+            'mediumseagreen': green,
+            'mediumslateblue': blue,
+            'mediumspringgreen': green,
+            'mediumturquoise': aqua,
+            'mediumvioletred': purple,
+            'midnightblue': blue,
+            'mintcream': aqua,
+            'mistyrose': pink,
+            'moccasin': beige,
+            'navajowhite': white,
+            'navy': blue,
+            'oldlace': beige,
+            'olive': green,
+            'olivedrab': green,
+            'orange': orange,
+            'orangered': orange,
+            'orchid': purple,
+            'palegoldenrod': golden,
+            'palegreen': green,
+            'paleturquoise': aqua,
+            'palevioletred': purple,
+            'papayawhip': beige,
+            'peachpuff': orange,
+            'peru': brown,
+            'pink': pink,
+            'plum': purple,
+            'powderblue': blue,
+            'purple': purple,
+            'red': red,
+            'rosybrown': brown,
+            'royalblue': blue,
+            'saddlebrown': brown,
+            'salmon': orange,
+            'sandybrown': brown,
+            'seagreen': green,
+            'seashell': white,
+            'sienna': orange,
+            'silver': grey,
+            'skyblue': blue,
+            'slateblue': blue,
+            'slategray': grey,
+            'slategrey': grey,
+            'snow': white,
+            'springgreen': green,
+            'steelblue': blue,
+            'tan': brown,
+            'teal': green,
+            'thistle': purple,
+            'tomato': red,
+            'turquoise': aqua,
+            'violet': purple,
+            'wheat': beige,
+            'white': white,
+            'whitesmoke': white,
+            'yellow': yellow,
+            'yellowgreen': yellow
+        }
+
         actual_name_color_ptbr = None
-        translator = Translator()
         if (actual_name_color_en != None):
-            actual_name_color_ptbr = translator.translate(actual_name_color_en,dest='pt',src='en').text
+            actual_name_color_ptbr = color_name_dict[actual_name_color_en]
             closest_name_color_ptbr = actual_name_color_ptbr
         else:
-            closest_name_color_ptbr = translator.translate(closest_name_color_en,dest='pt',src='en').text
+            closest_name_color_ptbr = color_name_dict[closest_name_color_en]
 
         return make_response(jsonify(ImageColorsResponse(actual_name_color_ptbr,closest_name_color_ptbr).__dict__), 200)
 
